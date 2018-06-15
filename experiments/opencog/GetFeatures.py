b4dd17e66b3f3a1bfac2bc13c6b66b7d8c9c2f8a
DaddyWesker <avenger15@yandex.ru>
2018-06-15 20:05:17 +0300

    GEtFeatures.py updated

--- experiments/opencog/GetFeatures.py
+++ experiments/opencog/GetFeatures.py
@@ -1,9 +1,12 @@
 import tensorflow.contrib.keras as ks
 from keras.applications.resnet50 import ResNet50
+from keras.applications import imagenet_utils
 import numpy as np
 
-def getFeatures(imgName):
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
 
+def getFeatures(imgName):
     feature_extractor = ResNet50(include_top=False)
     img = ks.preprocessing.image.load_img(imgName, target_size=(224,224))
     processedImage = np.asarray([ks.preprocessing.image.img_to_array(img)])
@@ -11,3 +14,26 @@ def getFeatures(imgName):
     features = feature_extractor.predict(processedImage)
     features = features.flatten()
     return features
+
+def findWord(inputList, Word):
+    idx = -1
+    for i in range (1000):
+        if inputList[0][i][1] == Word:
+            idx = i
+    if idx > -1:
+        return inputList[0][idx][2]
+    else:
+        return 0.0
+
+def predicate (features, ID):
+    base_model = ResNet50()
+    weights = base_model.get_layer(name='fc1000').get_weights()
+    result = np.matmul(np.transpose(features), weights[0])
+    result = softmax(result)
+    result = np.reshape(result, [-1, 1000])
+    P = imagenet_utils.decode_predictions(result, 1000)
+    probability = findWord(P, ID)
+    return probability
+
+def predicateHare (features):
+    return predicate(features, 'hare')
