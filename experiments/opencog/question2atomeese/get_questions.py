72464cd361d2719859edde383b026748fc543a96
Vitaly Bogdanov <vsbogd@gmail.com>
2018-06-27 19:11:59 +0300

    Add most frequent answer as right answer for question

--- experiments/opencog/question2atomeese/get_questions.py
+++ experiments/opencog/question2atomeese/get_questions.py
@@ -9,14 +9,28 @@ class Record:
         self.questionType = None
         self.questionId = None
         self.imageId = None
+        self.answer = None
 
     def toString(self):
-        return '{}:{}:{}:{}'.format(self.questionId, self.questionType, self.question, self.imageId);
+        return '{}::{}::{}::{}::{}'.format(self.questionId, self.questionType, self.question, self.imageId, self.answer);
+
+def getMostFrequentAnswer(answers):
+    maxCount = 0
+    maxAnswer = None
+    for answer, count in answers.items():
+        if count > maxCount:
+            maxCount = count
+            maxAnswer = answer
+    return maxAnswer
 
 test = False
 
 log = logging.getLogger("get_questions")
-log.setLevel(logging.INFO)
+if test:
+    log.setLevel(logging.DEBUG)
+else:
+    log.setLevel(logging.INFO)
+
 log.addHandler(logging.StreamHandler())
 
 parser = ijson.parse(open('v2_OpenEnded_mscoco_train2014_questions.json'))
@@ -47,17 +61,24 @@ parser = ijson.parse(open('v2_mscoco_train2014_annotations.json'))
 
 i = 0
 answerType = None
+answers = {}
 for prefix, event, value in parser:
     if test and i > 10:
         break
 
     log.debug('%s, %s, %s', prefix, event, value)
 
+    if (prefix == 'annotations.item.answers.item.answer'):
+        count = answers.setdefault(value, 0) + 1
+        answers[value] = count
+
     if (prefix == 'annotations.item.answer_type'):
         answerType = value
 
     if (prefix == 'annotations.item.question_id'):
         records[value].questionType = answerType
+        records[value].answer = getMostFrequentAnswer(answers)
+        answers = {}
         i += 1
 
 for questionId, record in records.items():
