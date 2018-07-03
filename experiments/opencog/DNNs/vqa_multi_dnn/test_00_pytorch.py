89b46343334fd2c9772aeede4e1987fce38e4b4e
Maxim Peterson <maxim.peterson@gmail.com>
2018-07-03 15:52:48 +0300

    test_00_pytorch_vqa_multi_dnn

---
+++ experiments/opencog/DNNs/vqa_multi_dnn/test_00_pytorch.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import vqa_data_parser as vqp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.autograd import Variable
+import os, sys, time, re
+
+
+pathVocabFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/yesno_predadj_words.txt'
+pathFeaturesTrainParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_parsed_features'
+pathFeaturesValParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_parsed_features'
+IMAGE_ID_FIELD_NAME = 'imageId'
+
+
+pathDataTrainFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_questions_parsed.txt'
+FILE_PREFIX = 'COCO_train2014_'
+
+pathDataValFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_questions_parsed.txt'
+FILE_PREFIX = 'COCO_val2014_'
+
+input_size = 2048
+nBBox = 36
+featOffset = 10
+nEpoch = 100
+learning_rate = 1e-3
+
+# Load vocabulary
+vocab = []
+with open(pathVocabFile, 'r') as filehandle:
+    vocab = [current_place.rstrip() for current_place in filehandle.readlines()]
+
+Nnets = len(vocab)
+
+def getWords(groundedFormula):
+    words = re.split(r', ', groundedFormula[groundedFormula.find("(") + 1:groundedFormula.find(")")])
+    return words
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class NetsVocab(nn.Module):
+    def __init__(self):
+        super(NetsVocab, self).__init__()
+        self.models = nn.ModuleList([nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+            ).to(device) for i in range(Nnets)])
+
+    def feed_forward(self, x, idx):
+        output = torch.ones(size=(nBBox,1)).to(device)
+        for k in idx:
+            logits = self.models[k](x)
+            # logits = model(x).view(-1)
+            predict = F.sigmoid(logits)
+            output = torch.mul(output, predict)
+        return output
+
+    def getParams(self, idx):
+        params=[]
+        for i in idx:
+            params.append({'params': self.models[i].parameters()})
+        return params
+
+
+nets = NetsVocab()
+
+
+# pathQuestFile_tab = '~/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/parsed_questions_tab.txt'
+# df_tab = pd.read_csv(pathQuestFile_tab, header=0, sep='\t',  low_memory=False)
+# df_quest = df_tab.loc[(df_tab['questionType'] == 'yes/no') & (df_tab['relexFormula'] == '_predadj(A, B)')]
+
+# Load bbox features
+# df = pd.read_csv(pathDataTrainFile, header=0, sep='\s*\::',  engine='python')
+df = pd.read_csv(pathDataValFile, header=0, sep='\s*\::',  engine='python')
+df_quest = df.loc[(df['questionType'] == 'yes/no') & (df['relexFormula'] == '_predadj(A, B)')]
+df_quest = df_quest.sort_values(['imageId'], ascending=[True])
+df_quest = df_quest.reset_index(drop=True)
+
+imgIdList = df_quest[IMAGE_ID_FIELD_NAME].tolist()
+
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!! HARDCODED INSIDE vpq.load_parsed_features !!!!
+data_feat =  vqp.load_parsed_features(pathFeaturesValParsed, imgIdList, filePrefix=FILE_PREFIX)
+
+# df.to_csv('parsed_yes_no_predadj.tsv', sep='\t', header=True, index=None)
+
+# Get list with binary answers yes = 1, no = 0
+nQuest = df_quest.shape[0]
+ansList = df_quest['answer'].tolist()
+for i in range(nQuest):
+    if ansList[i] == 'yes':
+        ansList[i] = 1
+    else:
+        ansList[i] = 0
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!!
+nQuest = len(data_feat)
+
+for e in range(nEpoch):
+    for i in range(nQuest):
+        idx = []
+        words = getWords(df_quest.loc[i, 'groundedFormula'])
+        nWords = len(words)
+        for w in words:
+            try:
+                idx.append( vocab.index(w) )
+            except ValueError:
+                continue
+
+        # get img bbox features
+        img_id = df_quest.loc[i, 'imageId']
+        ind = imgIdList.index(img_id)
+        f_input = data_feat[ind][1][:, featOffset:]
+        inputs = Variable(torch.Tensor(f_input)).to(device)
+
+        # ans = np.empty(shape=nBBox)
+        # ans.fill(ansList[i])
+
+        # Feed each bbox feature in the batch (36) to selected nets and multiply output probabilities
+        output = nets.feed_forward(inputs, idx)
+
+        ans = torch.from_numpy(np.asarray(ansList[i], dtype=np.float32)).to(device)
+        if (ans > 0):
+            output = torch.max(output).to(device)
+        else:
+            ans = torch.zeros((nBBox,1)).to(device)
+
+        loss = F.binary_cross_entropy(output, ans)
+
+        params = nets.getParams(idx)
+        optimizer = torch.optim.SGD(params, lr=learning_rate, momentum=0)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        rez_loss = loss.data.cpu().numpy()
+
+        sys.stdout.write("\r \r Training:\tepoch: {0}/{1}\tquestion: {2}/{3}\tloss: {4}".format( e, nEpoch, i, nQuest, rez_loss) )
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+    print("\n")
+
+print("Training is done!")
+
+
---
+++ experiments/opencog/DNNs/vqa_multi_dnn/vqa_data_parser.py
@@ -0,0 +1,89 @@
+
+from __future__ import print_function
+
+import os, sys, time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import base64
+import csv
+import h5py
+import _pickle as cPickle
+import numpy as np
+import math
+import pandas as pd
+
+
+csv.field_size_limit(sys.maxsize)
+
+FIELDNAMES = ['image_id', 'image_w', 'image_h', 'num_boxes', 'boxes', 'features']
+infile = 'data/trainval_36/trainval_resnet101_faster_rcnn_genome_36.tsv'
+
+FEATURESNAMES = ['roi_x', 'roi_y' 'roi_width' 'roi_height' 'spatial_feature_6d' 'img_feature_d2048']
+
+train_indices_file = 'data/train36_imgid2idx.pkl'
+val_indices_file = 'data/val36_imgid2idx.pkl'
+train_ids_file = 'data/train_ids.pkl'
+val_ids_file = 'data/val_ids.pkl'
+
+train_parsed_path = 'data/train2014_parsed_features'
+val_parsed_path = 'data/val2014_parsed_features'
+test_parsed_path = 'data/test2015_parsed_features'
+
+
+feature_length = 2048
+num_fixed_boxes = 36
+num_spatial_features = 6
+
+# FEATURESNAMES = ['roi_x', 'roi_y' 'roi_width' 'roi_height' 'spatial_feature_6d' 'img_feature_d2048']
+def load_parsed_features(pathFeatures, imgIDList, filePrefix = 'COCO_train2014_', id_len = 12):
+    data=[]
+
+    # Drop duplicates and sort
+    imgIDSet = sorted(set(imgIDList))
+    nImg = len(imgIDSet)
+
+    # !! FOR DEBUG LOAD ONLY 1% OF DATA
+    nImg = int(nImg / 100)
+
+    # Read parsed files and accumulate data
+    for i in range(nImg):
+        image_id = imgIDSet[i]
+        filePath = pathFeatures + '/' + filePrefix
+        nZeros = int((id_len - 1) - math.floor(math.log10(image_id)))
+        for _ in range(0, nZeros):
+            filePath = filePath + '0'
+
+        filePath = filePath + str(image_id) + '.tsv'
+
+        df = np.genfromtxt(filePath, delimiter='\t', skip_header=1)
+        data.append([image_id, df])
+
+        sys.stdout.write("\r \r Loading"
+                         " parsed features: {0}%\ti = {1}/{2}".format( (str(int(100 * float(i) / float(nImg)))), i, nImg ))
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+    print("\nFeatures loading is completed!")
+
+
+    return data
+
+
+def load_folder(folder, suffix):
+    imgs = []
+    for f in sorted(os.listdir(folder)):
+        if f.endswith(suffix):
+            imgs.append(os.path.join(folder, f))
+    return imgs
+
+
+def load_imageid(folder):
+    images = load_folder(folder, 'jpg')
+    img_ids = set()
+    for img in images:
+        img_id = int(img.split('/')[-1].split('.')[0].split('_')[-1])
+        img_ids.add(img_id)
+    return img_ids
+
+
+
