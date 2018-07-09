cdd8e6315e0a51d0d30d669f333851d81a6c9b00
Vitaly Bogdanov <vsbogd@gmail.com>
2018-07-09 20:25:15 +0300

    Add PoC of Atomeese NN training step.
    Tensors are passed via FloatValue using tensors cache. FloatValue
    contain a key to get tensor from cache. Evaluation result is represented
    in form (EvaluationLink (tv, tensor) (Predicate '<prediction|answer>')
    (ListLink <atomeese query> <bounding box>))

--- experiments/opencog/DNNs/training_steps/training_steps.py
+++ experiments/opencog/DNNs/training_steps/training_steps.py
@@ -1,13 +1,18 @@
 import logging
 import re
+import random
+import itertools
 
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from opencog.atomspace import AtomSpace
 from opencog.type_constructors import *
 from opencog.bindlink import evaluate_atom, execute_atom
+from asyncio.events import Handle
 
-class ModelVocabulary(nn.Module):
+class WordModelVocabulary:
     
     def __init__(self, words):
         super().__init__()
@@ -15,52 +20,113 @@ class ModelVocabulary(nn.Module):
         self.models = nn.ModuleList()
         self.modelIndexByWord = {}
         
+        global featureVectorSize, torchDevice
         modelIndex = 0
         for word in words:
             self.models.append(nn.Sequential(
-                nn.Linear(input_size, 64),
+                nn.Linear(featureVectorSize, 64),
                 nn.ReLU(),
                 nn.Linear(64, 32),
                 nn.ReLU(),
-                nn.Linear(32, 1)
-                ).to(device))
+                nn.Linear(32, 1),
+                nn.Sigmoid()
+                ).to(torchDevice))
             self.modelIndexByWord[word] = modelIndex
             ++modelIndex
     
     def getModelByWord(self, word):
         return self.models[self.modelIndexByWord[word]]
-#
-#     def getParams(self, idx):
-#         # TODO: fix
-#         params=[]
-#         for i in idx:
-#             params.append({'params': self.models[i].parameters()})
-#         return params
+
+class TensorCache:
+    
+    def __init__(self):
+        self.nextHandle = 0
+        self.cache = {}
+    
+    def addTensor(self, tensor):
+        handle = self.nextHandle
+        self.nextHandle += 1
+        self.cache[handle] = tensor
+        return handle
+    
+    def getTensor(self, handle):
+        return self.cache[handle]
+    
+    def clearTensor(self, handle):
+        del self.cache[handle]
+
+tensorCache = TensorCache()
+
+def getTensorValue(atom):
+    global tensorCache
+    tensorHandle = int(atom.get_value(PredicateNode('tensorHandle'))
+                       .to_list()[0])
+    tensor = tensorCache.getTensor(tensorHandle)
+    tensorCache.clearTensor(tensorHandle)
+    return tensor
+
+def setTensorValue(atom, tensor):
+    global tensorCache
+    handle = tensorCache.addTensor(tensor)
+    atom.set_value(PredicateNode('tensorHandle'), FloatValue(handle))
 
 def loadFeatures():
-    return []
+    global featureVectorSize
+    return list(map(lambda x: random.random(), range(featureVectorSize)))
 
-def buildModelFromAtomeese(modelCombination):
-    global models
-    combined = torch.ones(size=(nBBox,1)).to(device)
-    andLink = modelCombination
+def applyAtomeeseModel(atomeeseModel, features):
+    global wordModels, torchDevice
+    tensor = torch.ones(1).to(torchDevice)
+    andLink = atomeeseModel
     for conceptNode in andLink.get_out():
         # TODO: can be faster if regexp is compiled
-        match = re.search('^(.+)(?=.nn$)', conceptNode.get_name())
+        match = re.search('^(.+)(?=.nn$)', conceptNode.name)
         word = match.group(0)
-        model = models.getModelByWord(word)
-        combined = torch.max(torch.mul(nn.Sequential(model, nn.Sigmoid)), 0)
-    return combined
+        model = wordModels.getModelByWord(word)
+        tensor = torch.mul(model(features), tensor)
+    return tensor
 
-def runNeuralNetwork(modelCombination, featuresContainer):
-    finalModel = buildModelFromAtomeese(modelCombination)
-    features = featuresContainer.get_value(
-        PredicateNode('neuralNetworkFeatures'))
-    value = finalModel(features)
-    return TruthValue(value, 1.0)
+def runNeuralNetwork(atomeeseModel, featuresContainer):
+    features = torch.tensor(featuresContainer.get_value(
+        PredicateNode('neuralNetworkFeatures')).to_list())
+    tensor = applyAtomeeseModel(atomeeseModel, features)
+    result = EvaluationLink(PredicateNode('neuralNetworkPrediction'), 
+                             ListLink(atomeeseModel, featuresContainer))
+    result.tv = TruthValue(tensor.item(), 1.0)
+    setTensorValue(result, tensor)
+    return result
 
-def trainNeuralNetwork(modelCombination, predictedValue, expectedValue):
-    pass
+def createExpectedResult(atomeeseModel, featuresContainer, truthValue):
+    result = EvaluationLink(PredicateNode('expectedNeuralNetworkAnswer'), 
+                             ListLink(atomeeseModel, featuresContainer))
+    result.tv = TruthValue(truthValue, 1.0)
+    setTensorValue(result, torch.tensor([ truthValue ]))
+    return result
+
+def getAtomeeseModelParameters(atomeeseModel):
+    parameters = itertools.chain()
+    andLink = atomeeseModel
+    for conceptNode in andLink.get_out():
+        # TODO: can be faster if regexp is compiled
+        match = re.search('^(.+)(?=.nn$)', conceptNode.name)
+        word = match.group(0)
+        model = wordModels.getModelByWord(word)
+        parameters = itertools.chain(parameters, model.parameters())
+    return parameters
+
+def trainNeuralNetwork(atomeeseModel, predictedValue, expectedValue):
+    # TODO: model can be cached in predictedValue as well
+    parameters = getAtomeeseModelParameters(atomeeseModel)
+    # TODO: Maxim already implemented dynamic learning rate
+    optimizer = torch.optim.SGD(parameters, lr=1e-3, momentum=0)
+    optimizer.zero_grad()
+    loss = F.binary_cross_entropy(getTensorValue(predictedValue),
+                                  getTensorValue(expectedValue))
+    
+    loss.backward()
+    optimizer.step()
+    
+    return TruthValue(1.0, 1.0)
 
 def initLogger():
     global log
@@ -76,27 +142,30 @@ def initAtomspace():
 initLogger()
 initAtomspace()
 
-models = ModelVocabulary(['hare', 'grey'])
+featureVectorSize = 2048
+torchDevice = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+wordModels = WordModelVocabulary(['hare', 'grey'])
 
 boundingBox = ConceptNode("someBoundingBox")
 boundingBox.set_value(PredicateNode("neuralNetworkFeatures"), 
                       FloatValue(loadFeatures()))
 
-modelCombination = AndLink(ConceptNode('hare.nn'), ConceptNode('grey.nn'))
-log.info("modelCombination: %s", modelCombination)
+atomeeseModel = AndLink(ConceptNode('hare.nn'), ConceptNode('grey.nn'))
+log.info("atomeeseModel: %s", atomeeseModel)
 
-expectedValue = TruthValue(1.0, 1.0)
+expectedValue = createExpectedResult(atomeeseModel, boundingBox, 1.0)
 log.info('expectedValue: %s', expectedValue)
 
-predictedValue = evaluate_atom(atomspace, EvaluationLink(
-    GroundedPredicateNode('py:runNeuralNetwork'), 
-    ListLink(modelCombination, boundingBox)
+predictedValue = execute_atom(atomspace, ExecutionOutputLink(
+    GroundedSchemaNode('py:runNeuralNetwork'), 
+    ListLink(atomeeseModel, boundingBox)
     ))
 log.info('predictedValue: %s', predictedValue)
 
 # TODO: there is no Atomeese construction to call Python procedure 
 # but don't expect any result, i.e. call ```void foo()```
-execute_atom(atomspace, EvaluationLink(
+evaluate_atom(atomspace, EvaluationLink(
     GroundedPredicateNode('py:trainNeuralNetwork'), 
-    ListLink(modelCombination, predictedValue, expectedValue)
+    ListLink(atomeeseModel, predictedValue, expectedValue)
     ))
+log.info('train step finished')
