63ce7e01f6c31b99b3c1a96ceb9b5dc343fccc1c
Maxim Peterson <maxim.peterson@gmail.com>
2018-07-09 13:00:44 +0300

    new loss function

---
+++ experiments/opencog/DNNs/vqa_multi_dnn/eval_01_pytorch.py
@@ -0,0 +1,245 @@
+import pandas as pd
+import vqa_data_parser as vqp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.autograd import Variable
+import os, sys, time, re
+import math
+
+
+# FOR RUNNING ON K4
+# pathVocabFile = '/home/shared/datasets/yesno_predadj_words.txt'
+# pathFeaturesParsed = '/home/shared/datasets/VisualQA/Attention-on-Attention-data/val2014_parsed_features'
+# pathQuestFile = '/home/shared/datasets/val2014_questions_parsed.txt'
+# pathImgs = '/home/shared/datasets/val2014'
+
+
+#
+pathVocabFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/yesno_predadj_words.txt'
+
+pathImgs = '/home/mvp/Desktop/SingularityNET/my_exp/Attention-on-Attention-for-VQA/data/val2014'
+pathFeaturesParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_parsed_features'
+pathQuestFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_questions_parsed.txt'
+FILE_PREFIX = 'COCO_val2014_'
+
+# pathImgs = '/home/mvp/Desktop/SingularityNET/my_exp/Attention-on-Attention-for-VQA/data/train2014'
+# pathFeaturesParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_parsed_features'
+# pathQuestFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_questions_parsed.txt'
+# FILE_PREFIX = 'COCO_train2014_'
+
+
+IMAGE_ID_FIELD_NAME = 'imageId'
+id_len = 12
+
+
+pathSaveModel = './saved_models_01/99.16_67.65'
+
+isReduceSet = False
+
+input_size = 2048
+nBBox = 36
+featOffset = 10
+
+
+# Load vocabulary
+vocab = []
+with open(pathVocabFile, 'r') as filehandle:
+    vocab = [current_place.rstrip() for current_place in filehandle.readlines()]
+
+Nnets = len(vocab)
+
+def getWords(groundedFormula):
+    words = re.split(r', ', groundedFormula[groundedFormula.find("(") + 1:groundedFormula.find(")")])
+    return words
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class NetsVocab(nn.Module):
+    def __init__(self):
+        super(NetsVocab, self).__init__()
+        self.models = nn.ModuleList([nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+            ).to(device) for i in range(Nnets)])
+
+    def feed_forward(self, x, idx):
+        output = torch.ones(size=(nBBox,1)).to(device)
+        for k in idx:
+            logits = self.models[k](x)
+            # logits = model(x).view(-1)
+            predict = F.sigmoid(logits)
+            output = torch.mul(output, predict)
+        return output
+
+    def getParams(self, idx):
+        params=[]
+        for i in idx:
+            params.append({'params': self.models[i].parameters()})
+        return params
+
+print('Loading model...')
+nets = NetsVocab()
+checkpoint = torch.load(pathSaveModel + '/model_01_max_score_val.pth.tar')
+mean_loss = checkpoint['mean_loss']
+ep = checkpoint['epoch']
+nets.load_state_dict(checkpoint['state_dict'])
+print("Mean loss value: {} (epoch {})" .format(mean_loss, checkpoint['epoch']))
+
+
+# pathQuestFile_tab = '~/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/parsed_questions_tab.txt'
+# df_tab = pd.read_csv(pathQuestFile_tab, header=0, sep='\t',  low_memory=False)
+# df_quest = df_tab.loc[(df_tab['questionType'] == 'yes/no') & (df_tab['relexFormula'] == '_predadj(A, B)')]
+
+# Load bbox features
+#df = pd.read_csv(pathDataTrainFile, header=0, sep='\s*\::',  engine='python')
+df = pd.read_csv(pathQuestFile, header=0, sep='\s*\::',  engine='python')
+df_quest = df.loc[(df['questionType'] == 'yes/no') & (df['relexFormula'] == '_predadj(A, B)')]
+df_quest = df_quest.sort_values(['imageId'], ascending=[True])
+df_quest = df_quest.reset_index(drop=True)
+
+imgIdList = df_quest[IMAGE_ID_FIELD_NAME].tolist()
+# Drop duplicates and sort
+imgIdSet = sorted(set(imgIdList))
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!! HARDCODED INSIDE vpq.load_parsed_features !!!!
+data_feat =  vqp.load_parsed_features(pathFeaturesParsed, imgIdSet, filePrefix=FILE_PREFIX, reduce_set=isReduceSet)
+
+# df.to_csv('parsed_yes_no_predadj.tsv', sep='\t', header=True, index=None)
+
+# Get list with binary answers yes = 1, no = 0
+nQuest = df_quest.shape[0]
+questList = df_quest['question'].tolist()
+ansList = df_quest['answer'].tolist()
+ansListBin = []
+for i in range(nQuest):
+    if ansList[i] == 'yes':
+        ansListBin.append(1)
+    else:
+        ansListBin.append(0)
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!!
+nQuest = len(data_feat)
+min_loss = 1e8
+
+fileLog = open("log_val_01.txt", 'w')
+
+file = open("eval_01_results.html", 'w')
+file.write('<html>\n'
+           '<head>\n'
+           '<style>\n'
+           '.one{width: 700px;height: 700px;position: relative;}\n'
+           '.two{width: 700px;height: 700px;position: absolute;}\n'
+           '</style>\n'
+           '</head>\n'
+           '<body>\t<table>\n')
+
+score = 0.
+nQuestValid = 0
+ans_stat = []
+num_yes_gt = 0
+num_yes_pred = 0
+for i in range(nQuest):
+    idx = []
+    words = getWords(df_quest.loc[i, 'groundedFormula'])
+    nWords = len(words)
+    for w in words:
+        try:
+            idx.append( vocab.index(w) )
+        except ValueError:
+            continue
+
+    # get img bbox features
+    img_id = df_quest.loc[i, 'imageId']
+    filePath = pathImgs + '/' + FILE_PREFIX
+    nZeros = int((id_len - 1) - math.floor(math.log10(img_id)))
+    for _ in range(0, nZeros):
+        filePath = filePath + '0'
+
+    filePath = filePath + str(img_id) + '.jpg'
+
+
+    ind = imgIdSet.index(img_id)
+    bboxes = data_feat[ind][1][:, 0:4]
+    f_input = data_feat[ind][1][:, featOffset:]
+    inputs = Variable(torch.Tensor(f_input)).to(device)
+
+
+    # Feed each bbox feature in the batch (36) to selected nets and multiply output probabilities
+    output = nets.feed_forward(inputs, idx)
+
+    ans = np.asarray(ansListBin[i], dtype=np.float32)
+    _, idx_max = torch.max(output, 0)
+    imax = idx_max.data.cpu().numpy()
+
+    sum = torch.sum(output)
+    s = torch.div(output, sum)
+    sum_sq = torch.sum(torch.mul(output, output))
+    output = sum_sq / sum
+
+    output_max = output.data.cpu().numpy()
+
+    file.write('\t<tr><td><div class="one"><div class="two"><img src="' + filePath + '"/></div>')
+    file.write('')
+
+    for j in range(bboxes.shape[0]):
+    # for j in range(1):
+        x = int(bboxes[j][0])
+        y = int(bboxes[j][1])
+        width = int(bboxes[j][2])
+        height = int(bboxes[j][3])
+        color = 'blue'
+        if (j == imax[0]):
+            color = 'red'
+
+        file.write(
+            '<svg class="two"><rect x="{0}" y="{1}" width="{2}" height="{3}" stroke="{4}" fill="none"/></svg>'.format(x, y, width, height, color))
+
+    file.write('</div></td>\n')
+    file.write('<td><p> Question: ' + questList[i] + '</p></td>\n')
+    file.write('<td><p> Answer: ' + ansList[i] + '</p></td>\n')
+    file.write('<td><p> Prediction: ' + str(output_max) + '</p></td>\n')
+    file.write('\t</tr>\n')
+
+    abs_diff = math.fabs(ansListBin[i]-output_max)
+    if ( abs_diff <  0.5):
+        score += 1
+
+    ans_stat.append((ans, output_max))
+
+    if (ans > 0):
+        num_yes_gt += 1
+
+    if (output_max > 0.5):
+        num_yes_pred += 1
+
+    nQuestValid += 1
+
+    sys.stdout.write("\r \r Evaluation:\t{0}/{1}\t\tAccumulated score: {2}".format( i, nQuest, score ) )
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+file.write('</table>\n</body>\n<html>')
+file.close()
+
+fileLog.write("Number of validated questions: {}\n".format(nQuestValid))
+fileLog.write("Mean score is: {}\n".format(score/float(nQuestValid)))
+fileLog.write("GT answers stat:\tyes: {0}%\tno: {1}%\n".format( 100*float(num_yes_gt)/float(nQuestValid),
+                                                      100*(1-float(num_yes_gt)/float(nQuestValid))))
+fileLog.write("Predicted answers stat:\tyes: {0}%\tno: {1}%\n".format( 100*float(num_yes_pred)/float(nQuestValid),
+                                                             100*(1-float(num_yes_pred)/float(nQuestValid))))
+fileLog.close()
+
+print("\nEvaluation is done!")
+print("Mean score is: {}".format(score/float(nQuestValid)))
+print("GT answers stat:\tyes: {0}%\tno: {1}%".format( 100*float(num_yes_gt)/float(nQuestValid),
+                                                      100*(1-float(num_yes_gt)/float(nQuestValid))))
+print("Predicted answers stat:\tyes: {0}%\tno: {1}%".format( 100*float(num_yes_pred)/float(nQuestValid),
+                                                             100*(1-float(num_yes_pred)/float(nQuestValid))))
+
---
+++ experiments/opencog/DNNs/vqa_multi_dnn/train_01_pytorch.py
@@ -0,0 +1,314 @@
+import pandas as pd
+import vqa_data_parser as vqp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.autograd import Variable
+import os, sys, time, re
+import math
+
+# FOR RUNNING ON K4
+# pathVocabFile = '/home/shared/datasets/yesno_predadj_words.txt'
+# pathFeaturesTrainParsed = '/home/shared/datasets/VisualQA/Attention-on-Attention-data/train2014_parsed_features'
+# pathFeaturesValParsed = '/home/shared/datasets/VisualQA/Attention-on-Attention-data/val2014_parsed_features'
+# pathDataTrainFile = '/home/shared/datasets/train2014_questions_parsed.txt'
+# pathDataValFile = '/home/shared/datasets/val2014_questions_parsed.txt'
+
+
+pathVocabFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/yesno_predadj_words.txt'
+pathFeaturesTrainParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_parsed_features'
+pathFeaturesValParsed = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_parsed_features'
+pathDataTrainFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/train2014_questions_parsed.txt'
+pathDataValFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_questions_parsed.txt'
+
+pathSaveModel = './saved_models'
+FILE_PREFIX_TRAIN = 'COCO_train2014_'
+FILE_PREFIX_VAL = 'COCO_val2014_'
+IMAGE_ID_FIELD_NAME = 'imageId'
+
+
+# pathFeaturesTrainParsed = pathFeaturesValParsed
+# pathDataTrainFile = '/home/mvp/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/val2014_questions_parsed.txt'
+# FILE_PREFIX = 'COCO_val2014_'
+
+input_size = 2048
+nBBox = 36
+featOffset = 10
+nEpoch = 500
+learning_rate = 1e-2
+lr_decay_iter = 30
+
+isReduceSet = False
+
+# Load vocabulary
+vocab = []
+with open(pathVocabFile, 'r') as filehandle:
+    vocab = [current_place.rstrip() for current_place in filehandle.readlines()]
+
+Nnets = len(vocab)
+
+def getWords(groundedFormula):
+    words = re.split(r', ', groundedFormula[groundedFormula.find("(") + 1:groundedFormula.find(")")])
+    return words
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class NetsVocab(nn.Module):
+    def __init__(self):
+        super(NetsVocab, self).__init__()
+        self.models = nn.ModuleList([nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+            ).to(device) for i in range(Nnets)])
+
+    def feed_forward(self, x, idx):
+        output = torch.ones(size=(nBBox,1)).to(device)
+        for k in idx:
+            logits = self.models[k](x)
+            # logits = model(x).view(-1)
+            predict = F.sigmoid(logits)
+            output = torch.mul(output, predict)
+        return output
+
+    def getParams(self, idx):
+        params=[]
+        for i in idx:
+            params.append({'params': self.models[i].parameters()})
+        return params
+
+
+nets = NetsVocab()
+
+# Continue training from saved checkpoint
+# checkpoint = torch.load(pathSaveModel + '/model_bkp.pth.tar')
+# mean_loss = checkpoint['mean_loss']
+# ep = checkpoint['epoch']
+# nets.load_state_dict(checkpoint['state_dict'])
+
+
+# Prepare training data
+
+# pathQuestFile_tab = '~/Desktop/SingularityNET/datasets/VisualQA/balanced_real_images/parsed_questions_tab.txt'
+# df_tab = pd.read_csv(pathQuestFile_tab, header=0, sep='\t',  low_memory=False)
+# df_quest = df_tab.loc[(df_tab['questionType'] == 'yes/no') & (df_tab['relexFormula'] == '_predadj(A, B)')]
+
+# Load bbox features
+df = pd.read_csv(pathDataTrainFile, header=0, sep='\s*\::',  engine='python')
+# df = pd.read_csv(pathDataValFile, header=0, sep='\s*\::',  engine='python')
+df_quest = df.loc[(df['questionType'] == 'yes/no') & (df['relexFormula'] == '_predadj(A, B)')]
+df_quest = df_quest.sort_values(['imageId'], ascending=[True])
+df_quest = df_quest.reset_index(drop=True)
+
+imgIdList = df_quest[IMAGE_ID_FIELD_NAME].tolist()
+# Drop duplicates and sort
+imgIdSet = sorted(set(imgIdList))
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!! HARDCODED INSIDE vpq.load_parsed_features !!!!
+data_feat =  vqp.load_parsed_features(pathFeaturesTrainParsed, imgIdSet, filePrefix=FILE_PREFIX_TRAIN, reduce_set=isReduceSet)
+
+
+# Get list with binary answers yes = 1, no = 0
+nQuest = df_quest.shape[0]
+ansList = df_quest['answer'].tolist()
+num_yes_gt = 0
+for i in range(nQuest):
+    if ansList[i] == 'yes':
+        ansList[i] = 1
+        num_yes_gt += 1
+    else:
+        ansList[i] = 0
+#########################################################################
+
+# Prepare validation data
+
+# Load bbox features
+#df = pd.read_csv(pathDataTrainFile, header=0, sep='\s*\::',  engine='python')
+df_val = pd.read_csv(pathDataValFile, header=0, sep='\s*\::',  engine='python')
+df_quest_val = df_val.loc[(df_val['questionType'] == 'yes/no') & (df_val['relexFormula'] == '_predadj(A, B)')]
+df_quest_val = df_quest_val.sort_values(['imageId'], ascending=[True])
+df_quest_val = df_quest_val.reset_index(drop=True)
+
+imgIdList_val = df_quest_val[IMAGE_ID_FIELD_NAME].tolist()
+# Drop duplicates and sort
+imgIdSet_val = sorted(set(imgIdList_val))
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!! HARDCODED INSIDE vpq.load_parsed_features !!!!
+data_feat_val =  vqp.load_parsed_features(pathFeaturesValParsed, imgIdSet_val, filePrefix=FILE_PREFIX_VAL, reduce_set=isReduceSet)
+
+nQuest_val = df_quest_val.shape[0]
+questList_val = df_quest_val['question'].tolist()
+ansList_val = df_quest_val['answer'].tolist()
+ansListBin_val = []
+for i in range(nQuest_val):
+    if ansList_val[i] == 'yes':
+        ansListBin_val.append(1)
+    else:
+        ansListBin_val.append(0)
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!!
+nQuest_val = len(data_feat_val)
+
+##########################################################################
+
+
+
+fileLog = open('log_train_01.txt', 'w')
+fileLog.write("Number of training questions: {}\n".format(nQuest))
+fileLog.write("Number of training images: {}\n".format(len(imgIdSet)))
+fileLog.write("GT answers stat:\tyes: {0}%\tno: {1}%\n".format( 100*float(num_yes_gt)/float(nQuest),
+                                                      100*(1-float(num_yes_gt)/float(nQuest))))
+
+fileLogNumbers = open('log_train_01_nums.txt', 'w')
+fileLogNumbers.write("# epoch\tmean_loss\ttrain_score\tval_score\n")
+
+# !! FOR DEBUG LOAD ONLY 1% OF DATA !!!
+nQuest = len(data_feat)
+min_loss = 1e8
+max_score_val = 0
+lr = learning_rate
+for e in range(nEpoch):
+    mean_loss = 0.
+    score = 0
+    nQuestValid = 0
+
+    for i in range(nQuest):
+        idx = []
+        words = getWords(df_quest.loc[i, 'groundedFormula'])
+        nWords = len(words)
+        for w in words:
+            try:
+                idx.append( vocab.index(w) )
+            except ValueError:
+                continue
+
+        # get img bbox features
+        img_id = df_quest.loc[i, 'imageId']
+        # ind = imgIdList.index(img_id)
+        ind = imgIdSet.index(img_id)
+        f_input = data_feat[ind][1][:, featOffset:]
+        inputs = Variable(torch.Tensor(f_input)).to(device)
+
+
+        # Feed each bbox feature in the batch (36) to selected nets and multiply output probabilities
+        output = nets.feed_forward(inputs, idx)
+
+        ans = torch.from_numpy(np.asarray(ansList[i], dtype=np.float32)).to(device)
+
+        sum = torch.sum(output)
+        s = torch.div(output, sum)
+        sum_sq = torch.sum(torch.mul(output, output))
+        output = sum_sq / sum
+
+        loss = F.binary_cross_entropy(output, ans)
+
+        params = nets.getParams(idx)
+
+        # adjust learning rate
+        if e % lr_decay_iter == 0:
+            lr = learning_rate * (1 - float(e) / float(nEpoch)) ** 0.9
+
+        optimizer = torch.optim.SGD(params, lr=lr, momentum=0)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        res_loss = loss.data.cpu().numpy()
+        mean_loss += res_loss
+
+        res_predict = np.max( output.data.cpu().numpy() )
+        abs_diff = np.fabs(ansList[i] - res_predict)
+        if ( abs_diff < 0.5):
+            score += 1
+
+        sys.stdout.write("\r \r Training:\tepoch: {0}/{1}\tquestion: {2}/{3}\tloss: {4}".format( e, nEpoch, i, nQuest, res_loss) )
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+        nQuestValid += 1
+
+    mean_loss /= float(nQuestValid)
+    score = score/float(nQuestValid)
+    print("\nEpoch: {0}/{1}\t lr: {2}\tMean loss: {3}\tScore: {4}%\n".format( e, nEpoch, lr, mean_loss, 100*score))
+    fileLog.write("Epoch: {0}/{1}\tlr: {2}\tMean loss: {3}\tScore: {4}%\n".format(e, nEpoch, lr, mean_loss, 100 * score))
+
+    # Validation
+    if (e % 10) == 0:
+        score_val = 0.
+        nQuestValid_val = 0
+
+        for i in range(nQuest_val):
+            idx_val = []
+            words_val = getWords(df_quest_val.loc[i, 'groundedFormula'])
+            nWords_val = len(words_val)
+            for w in words_val:
+                try:
+                    idx_val.append(vocab.index(w))
+                except ValueError:
+                    continue
+
+            # get img bbox features
+            img_id = df_quest_val.loc[i, 'imageId']
+
+            ind_val = imgIdSet_val.index(img_id)
+            bboxes = data_feat_val[ind_val][1][:, 0:4]
+            f_input_val = data_feat_val[ind_val][1][:, featOffset:]
+            inputs_val = Variable(torch.Tensor(f_input_val)).to(device)
+
+            # Feed each bbox feature in the batch (36) to selected nets and multiply output probabilities
+            output_val = nets.feed_forward(inputs_val, idx_val)
+
+            sum_val = torch.sum(output_val)
+            s_val = torch.div(output, sum_val)
+            sum_sq_val = torch.sum(torch.mul(output_val, output_val))
+            output_val = sum_sq_val / sum_val
+
+            output_max_val = output_val.data.cpu().numpy()
+            abs_diff = math.fabs(ansListBin_val[i] - output_max_val)
+            if (abs_diff < 0.5):
+                score_val += 1
+
+            nQuestValid_val += 1
+
+            sys.stdout.write("\r \r Evaluation:\t{0}/{1}\t\tAccumulated score: {2}".format(i, nQuest_val, score_val))
+            sys.stdout.flush()
+            time.sleep(0.01)
+
+        score_val = score_val / float(nQuestValid_val)
+
+        print("\nEvaluation is done!")
+        print("Mean score is: {}%\n".format(100*score_val))
+
+        fileLog.write(
+            "\n Evaluation at {0}/{1} epoch gives score:\t{2}%\n\n".format(e, nEpoch, 100 * score_val))
+
+        fileLogNumbers.write("{0}\t{1}\t{2}\t{3}\n".format(e, mean_loss, 100*score, 100*score_val))
+
+        if (max_score_val < score_val):
+            state = {'epoch': e, 'state_dict': nets.state_dict(), 'optimizer': optimizer.state_dict(),
+                     'mean_loss': mean_loss}
+            filename = pathSaveModel + '/model_01_max_score_val.pth.tar'
+            torch.save(state, filename)
+
+            fileLog.write(
+                "\n Saving model at {0}/{1} epoch with val_score: {2}%\n\n".format(e, nEpoch, 100 * score_val))
+
+            max_score_val = score_val
+
+    if (mean_loss < min_loss):
+        state = {'epoch': e, 'state_dict': nets.state_dict(), 'optimizer' : optimizer.state_dict(), 'mean_loss' : mean_loss }
+        filename = pathSaveModel + '/model_01.pth.tar'
+        torch.save(state, filename)
+
+        fileLog.write("\n Saving model at {0}/{1} epoch with mean loss: {2}\tscore: {3}%\n\n".format(e, nEpoch, mean_loss, 100 * score))
+
+fileLogNumbers.close()
+fileLog.close()
+print("Training is done!")
+
