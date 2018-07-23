b0e356c3508e76d6425590a85f9406182fd6da1c
Vitaly Bogdanov <vsbogd@gmail.com>
2018-07-23 19:12:02 +0300

    Use URE to calculate gradient.
    Max iterations count added. Each arithmetic operation is wrapped by
    (ListLink ...) to fix the rule.

--- experiments/opencog/backprop/URE_autodiff/AutoDiffTest.scm
+++ experiments/opencog/backprop/URE_autodiff/AutoDiffTest.scm
@@ -1,4 +1,4 @@
-(add-to-load-path "/usr/share/opencog/scm") ; !!!! check this path
+;(add-to-load-path "/usr/share/opencog/scm") ; !!!! check this path
 (add-to-load-path ".")
 
 (use-modules (opencog))
@@ -13,15 +13,35 @@
        (VariableNode "$F1")
        (VariableNode "$F2"))
       (AndLink
-      (ListLink (ConceptNode "Gradient") (ConceptNode "*") (VariableNode "$F1") (VariableNode "$F2")))
-      (ListLink (ConceptNode "+") (ListLink (ConceptNode "*") (VariableNode "$F2") 
+      (ListLink (ConceptNode "Gradient") (ListLink (ConceptNode "*") (VariableNode "$F1") (VariableNode "$F2"))))
+
+      (ListLink (ConceptNode "+")
+		(ListLink (ConceptNode "*") (VariableNode "$F2") 
                                             (ListLink (ConceptNode "Gradient") (VariableNode "$F1")))
                 (ListLink (ConceptNode "*") (VariableNode "$F1") 
-                                            (ListLink (ConceptNode "Gradient") (VariableNode "$F2"))))))
+                                            (ListLink (ConceptNode "Gradient") (VariableNode "$F2")))
+      )
+      
+      ))
 
 
-(ListLink (ConceptNode "Gradient") (ConceptNode "*") (ListLink (ConceptNode "+") (NumberNode 3) (NumberNode 3))
-          (ListLink (ConceptNode "*") (NumberNode 3) (NumberNode 3)))
+(define SRC (ListLink (ConceptNode "Gradient") (ListLink (ConceptNode "*") (ListLink (ConceptNode "+") (NumberNode 1) (NumberNode 2))
+          (ListLink (ConceptNode "*") (NumberNode 3) (NumberNode 4)))) )
+;(define SRC (ListLink (ConceptNode "Gradient") (ListLink (ConceptNode "*") (NumberNode 1) (NumberNode 2))))
+
+(ExecutionLink
+   (SchemaNode "URE:maximum-iterations")
+   (ConceptNode "my-rule-base")
+   (NumberNode "10000")
+   )
+
+(Inheritance (Concept "my-rule-base") (Concept "URE"))
+
+(Member (DefinedSchema "GradientMul" (stv 0.4 1)) (Concept "my-rule-base"))
+
+(define (my-forward-chainer SRC) (cog-fc (Concept "my-rule-base") SRC))
+
+(GetLink (VariableNode "$X"))
 
 (Inheritance (Concept "my-rule-base") (Concept "URE"))
 
