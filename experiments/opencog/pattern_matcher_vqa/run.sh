18ce130bc1a2ab4cab49638100eebcfe32902b70
Anatoly Belikov <awbelikov@gmail.com>
2018-09-11 13:36:11 +0300

    change paths in run.sh

--- experiments/opencog/pattern_matcher_vqa/run.sh
+++ experiments/opencog/pattern_matcher_vqa/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 python3 pattern_matcher_vqa.py \
-    --questions filtered_questions.txt \
+    --questions /mnt/fileserver/shared/models/vqa_split_multidnn/questions.txt \
     --model-kind SPLITMULTIDNN \
-    --atomspace atomspace_val.scm \
-    --multidnn-model /home/noskill/projects/models/multi/visual_genome \
+    --atomspace /mnt/fileserver/shared/models/vqa_split_multidnn/vqa_dataset/atomspace_val.scm \
+    --multidnn-model /mnt/fileserver/shared/models/vqa_split_multidnn/visual_genome \
     --features-extractor-kind PRECALCULATED \
-    --precalculated-features /home/noskill/projects/models/features/val2014_parsed_features.zip \
+    --precalculated-features /mnt/fileserver/shared/datasets/at-on-at-data/val2014_parsed_features.zip \
     --precalculated-features-prefix val2014_parsed_features/COCO_val2014_ \
     --python-log-level INFO \
     --opencog-log-level INFO 
