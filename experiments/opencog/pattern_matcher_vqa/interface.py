3bae800b37711a36a8f1f328b1763ce23e01a2d3
Anatoly Belikov <awbelikov@gmail.com>
2018-09-25 16:08:56 +0300

    style

--- experiments/opencog/pattern_matcher_vqa/interface.py
+++ experiments/opencog/pattern_matcher_vqa/interface.py
@@ -2,40 +2,40 @@ from abc import ABC
 
 
 class NeuralNetworkRunner(ABC):
-    
+
     def runNeuralNetwork(self, features, word):
         pass
 
 
 class AnswerHandler(ABC):
-    
+
     def onNewQuestion(self, record):
         pass
-    
+
     def onAnswer(self, record, answer):
         pass
 
-    def get_unanswered(self):
+    def getUnanswered(self):
         return list()
 
 
 class ChainAnswerHandler(AnswerHandler):
-    
+
     def __init__(self, answerHandlerList):
         self.answerHandlerList = answerHandlerList
-        
+
     def onNewQuestion(self, record):
         self.notifyAll(lambda handler: handler.onNewQuestion(record))
 
     def onAnswer(self, record, answer):
         self.notifyAll(lambda handler: handler.onAnswer(record, answer))
-        
+
     def notifyAll(self, methodToCall):
         map(methodToCall, answerHandlerList)
 
 
 class FeatureExtractor(ABC):
-    
+
     def getFeaturesByImageId(self, imageId):
         pass
 
