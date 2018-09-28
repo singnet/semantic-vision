24c8b9fc16ae29938bae373f41f5808a5404c2ae
Anatoly Belikov <awbelikov@gmail.com>
2018-09-28 19:16:38 +0300

    use ure in example script

--- experiments/opencog/pattern_matcher_vqa/answer_by_image.py
+++ experiments/opencog/pattern_matcher_vqa/answer_by_image.py
@@ -13,19 +13,29 @@ def test():
     prototxt = '/mnt/fileserver/shared/vital/image-features/test.prototxt'
     caffemodel = '/mnt/fileserver/shared/vital/image-features/resnet101_faster_rcnn_final_iter_320000_for_36_bboxes.caffemodel'
     models = '/mnt/fileserver/shared/models/vqa_split_multidnn/visual_genome/'
-    atomspace_path = '/mnt/fileserver/shared/models/vqa_split_multidnn/vqa_dataset/atomspace_val.scm' 
+    atomspace_path = '/mnt/fileserver/shared/models/vqa_split_multidnn/vqa_dataset/atomspace_val.scm'
     jpype.startJVM(jpype.getDefaultJVMPath(),
                    '-Djava.class.path=' + question2atomeseLibraryPath)
 
     question_converter = jpype.JClass('org.opencog.vqa.relex.QuestionToOpencogConverter')()
     extractor = ImageFeatureExtractor(prototxt, caffemodel)
-    atomspace = initialize_atomspace_by_facts()
-    image =  misc.imread('images/Fat-Zebra-Animated-Animal-Photo.jpg') 
+
+    scheme_directories = ["/home/noskill/projects/opencog/examples/pln/conjunction/",
+                          "/home/noskill/projects/atomspace/examples/rule-engine/rules/",
+                          "/home/noskill/projects/opencog/opencog/pln/rules/"]
+
+    atomspace = initialize_atomspace_by_facts("atomspace_val.scm",
+                                              "conjunction-rule-base-config.scm",
+                                              scheme_directories)
+    image =  misc.imread('images/Fat-Zebra-Animated-Animal-Photo.jpg')
     network_runner.runner = SplitMultidnnRunner(models)
-    vqa = PatternMatcherVqaPipeline(extractor, question_converter, atomspace, None) 
-    print(vqa.answerQuestionByImage(image, 'Are zebras fat?'))
+    vqa = PatternMatcherVqaPipeline(extractor, question_converter, atomspace, None)
+    print(vqa.answerQuestionByImage(image, 'Are zebras striped?'))
+    print(vqa.answerQuestionByImage(image, 'Are zebras striped?', use_pm=False))
 
-
+    image =  misc.imread('images/red-plane.jpg')
+    print(vqa.answerQuestionByImage(image, 'What color is the plane?'))
+    print(vqa.answerQuestionByImage(image, 'What color is the plane?', use_pm=False))
 
 if __name__ == '__main__':
     test()
