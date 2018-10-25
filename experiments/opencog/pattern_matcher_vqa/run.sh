b3c3c2225ddd76a2c4c18c82a53b2b04d85dcf0b
Anatoly Belikov <awbelikov@gmail.com>
2018-10-25 17:35:07 +0300

    use relative paths

--- experiments/opencog/pattern_matcher_vqa/run.sh
+++ experiments/opencog/pattern_matcher_vqa/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 python3 pattern_matcher_vqa.py \
-    --questions /mnt/fileserver/shared/models/vqa_split_multidnn/questions.txt \
+    --questions ~/projects/data/questions.934.txt \
     --model-kind SPLITMULTIDNN \
-    --atomspace /mnt/fileserver/shared/models/vqa_split_multidnn/vqa_dataset/atomspace_val.scm \
-    --multidnn-model /mnt/fileserver/shared/models/vqa_split_multidnn/visual_genome \
+    --atomspace ~/projects/data/train_tv_atomspace.scm \
+    --multidnn-model ~/projects/data/visual_genome \
     --features-extractor-kind PRECALCULATED \
-    --precalculated-features /mnt/fileserver/shared/datasets/at-on-at-data/val2014_parsed_features.zip \
+    --precalculated-features ~/projects/data/val2014_parsed_features.zip \
     --precalculated-features-prefix val2014_parsed_features/COCO_val2014_ \
     --python-log-level INFO \
     --opencog-log-level INFO 
