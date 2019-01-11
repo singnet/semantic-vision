1f791755f422787d33b6da844c35213610b38c7b
Vitaly Bogdanov <vsbogd@gmail.com>
2019-01-11 18:20:10 +0300

    Add PtrValue Python type constructor

--- experiments/opencog/opencog-neuralnet/cython/neuralnet/ptrvalue.pyx
+++ experiments/opencog/opencog-neuralnet/cython/neuralnet/ptrvalue.pyx
@@ -4,10 +4,14 @@ cdef Value wrapPtrValue(cValuePtr shared_ptr):
     """Factory method to construct PtrValue from C++ ValuePtr (see
     http://docs.cython.org/en/latest/src/userguide/extension_types.html#instantiation-from-existing-c-c-pointers
     for example)"""
-    cdef PtrValue value = PtrValue.__new__(PtrValue)
+    cdef PtrValueClass value = PtrValueClass.__new__(PtrValueClass)
     value.shared_ptr = shared_ptr
     return value
 
-cdef class PtrValue(Value):
+cdef class PtrValueClass(Value):
     def value(self):
         return <object>(<cPtrValue*>get_value_ptr(self)).value()
+
+def PtrValue(obj):
+    incref(<void*>obj)
+    return wrapPtrValue(createPtrValue(<void*>obj, decref))
