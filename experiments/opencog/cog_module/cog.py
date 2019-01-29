0e0fed222d1fa3e0e12fdd023721203d681d25a3
Necr0x0Der <pas.aicv@gmail.com>
2019-01-29 18:27:39 +0300

    updated example with cog module

--- experiments/opencog/cog_module/cog.py
+++ experiments/opencog/cog_module/cog.py
@@ -34,6 +34,11 @@ class cogModule(torch.nn.Module):
         return getattr(obj, methodname.name)(args)
 
     @staticmethod
+    def newLink(atom):
+        print("HERE: ", atom)
+        return atom
+
+    @staticmethod
     def Execute(atom, *args):
         return ExecutionOutputLink(
             GroundedSchemaNode("py:cogModule.callMethod"),
@@ -53,6 +58,9 @@ class cogModule(torch.nn.Module):
     #todo: same names with static Execute ?
     def Exec(self, *args):
         return cogModule.Execute(self.atom, *args)
+
+    def Eval(self, *args):
+        return cogModule.Evaluate(self.atom, *args)
 
     def execute(self, args):
         #print("Args: ", args)
@@ -112,12 +120,103 @@ prog2 = net2.Exec(inp.Exec())
 print(get_cached_value(execute_atom(atomspace, prog2)))
 
 bl = BindLink(
-    #TypedVariableNode(VariableNode("$X"), TypeNode("ConceptNode")),
-    VariableNode("$X"),
+    TypedVariableLink(VariableNode("$X"), TypeNode("ConceptNode")),
+    #VariableNode("$X"),
     AndLink(
         InheritanceLink(VariableNode("$X"), ConceptNode("color")),
         cogModule.Evaluate(VariableNode("$X"), inp.Exec()) #inp.Exec() == cogModule.Execute(ConceptNode("image"))
     ),
     VariableNode("$X")
 )
+print(bindlink(atomspace, bl))
+
+# ----------------
+
+# wrapping torch.tensor tv to make it trainable
+# class TensorTVModule(cogModule):
+class InheritanceModule(cogModule):
+    def __init__(self, atom, init_tv):
+        super().__init__(atom)
+        self.tv = init_tv
+    def forward(self):
+        return self.tv
+
+class AndModule(cogModule):
+    def forward(self, a, b):
+        return a * b # torch.min(a, b)
+
+# inheritance links are concrete links... we can bind specific objects to them
+h = InheritanceLink(ConceptNode("red"), ConceptNode("color"))
+InheritanceModule(h, torch.tensor([0.95]))
+h = InheritanceLink(ConceptNode("green"), ConceptNode("color"))
+InheritanceModule(h, torch.tensor([0.93]))
+# doesn't work with Evaluate
+print("Tensor truth value: ", get_cached_value(execute_atom(atomspace,
+    cogModule.Execute(InheritanceLink(ConceptNode("green"), ConceptNode("color"))))))
+
+# AndLinks are created dynamically, in ad hoc fashion for queries, etc.
+# it's cumbersome and unnecesary to bind them to individual objects
+# h = AndLink(?, ?)
+and_net = AndModule(ConceptNode("AndLink")) # GroundedObjectNode would be suitable here
+
+bl = BindLink(
+    VariableNode("$X"),
+    #TypedVariableLink(VariableNode("$X"), TypeNode("ConceptNode")),
+    and_net.Eval(
+        cogModule.Execute(InheritanceLink(VariableNode("$X"), ConceptNode("color"))),
+        cogModule.Execute(VariableNode("$X"), inp.Exec())
+    ),
+    VariableNode("$X")
+)
+# doesn't work, because Pattern Matcher doesn't see cogModule.Evaluate(InheritanceLink...) and cogModule.Evaluate(VariableNode...) as clauses,
+# so no restrictions on $X
+#print(bindlink(atomspace, bl))
+
+
+bl = BindLink(
+    VariableNode("$X"),
+    AndLink(
+        InheritanceLink(VariableNode("$X"), ConceptNode("color")),
+        and_net.Eval(
+            cogModule.Execute(InheritanceLink(VariableNode("$X"), ConceptNode("color"))),
+            cogModule.Execute(VariableNode("$X"), inp.Exec())
+        )
+    ),
+    # another bad thing is that we need to execute it once again; caching might help though
+    and_net.Eval(
+            cogModule.Execute(InheritanceLink(VariableNode("$X"), ConceptNode("color"))),
+            cogModule.Execute(VariableNode("$X"), inp.Exec()))
+)
+print(bl)
+print(bindlink(atomspace, bl))
+# somehow works, but:
+# - Execute should be used instead of Evaluate in internal calls - that's ok, and
+# - looks somewhat artificial
+# - and_net.Eval result is lost... (and it is assigned to Node, so we get recover only one value for all results...)
+# ==> URE is necessary
+
+AndLink(
+    InheritanceLink(ConceptNode("a"), ConceptNode("b")),
+    EvaluationLink(PredicateNode("c"), ConceptNode("d")))
+
+bl = BindLink(
+    VariableList(
+        TypedVariableLink(VariableNode("$X"), TypeNode("InheritanceLink")),
+        TypedVariableLink(VariableNode("$Y"), TypeNode("EvaluationLink"))),
+        #TypedVariableLink(VariableNode("$X"), TypeChoice(TypeNode("ExecutionOutputLink"), TypeNode("InheritanceLink"))),
+        #TypedVariableLink(VariableNode("$Y"), TypeChoice(TypeNode("ExecutionOutputLink"), TypeNode("InheritanceLink")))),
+    AndLink(
+        AndLink(
+            VariableNode("$X"),
+            VariableNode("$Y")
+        )
+    ),
+    ExecutionOutputLink(
+        GroundedSchemaNode("py:cogModule.newLink"),
+        #ListLink(QuoteLink(AndLink(VariableNode("$X"), VariableNode("$Y"))))
+        #AndLink(VariableNode("$X"), VariableNode("$Y"))
+        ListLink(VariableNode("$Y"))
+    )
+)
+
 bindlink(atomspace, bl)
