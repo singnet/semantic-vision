828de2cbe455ccb4e8fb58691dafa84f14d5f580
Anatoly Belikov <awbelikov@gmail.com>
2019-02-05 11:24:58 +0300

    remove caching

--- experiments/opencog/cog_module/module.py
+++ experiments/opencog/cog_module/module.py
@@ -1,4 +1,5 @@
 import torch
+import uuid
 from torch.distributions import normal
 
 # Experimental design of cog.Module API for running opencog reasoning from pytorch nn.Module extension
@@ -11,18 +12,30 @@ from opencog.type_constructors import *
 def get_cached_value(atom):
     # can be generalized, e.g. ConceptNodes can be converted to their string names,
     # so string can be an argument to forward, while ConceptNode can be an argument to execute
-    value = atom.get_value(atom.atomspace.add_node(types.PredicateNode, "cogNet"))
+    key = atom.atomspace.add_node(types.PredicateNode, "cogNet")
+    value = atom.get_value(key)
     if value is None:
         raise RuntimeError("atom {0} has no value for {1}".format(str(atom), str(key)))
     result = valueToPtrValue(value).value()
+    return result
     return getattr(result, 'cached_result', result)
+
+def set_value(atom, value):
+    key = atom.atomspace.add_node(types.PredicateNode, "cogNet")
+    atom.set_value(key, PtrValue(value))
 
 
 def unpack_args(*atoms):
     return (get_cached_value(atom) for atom in atoms)
 
 
-def evaluate(atom, *args):
+def _wrap_in_list(*args, wrap_args=True):
+    if wrap_args:
+        return ListLink(*args)
+    return args
+
+
+def evaluate(atom, *args, wrap_args=True):
     return EvaluationLink(
         GroundedPredicateNode("py:CogModule.callMethod"),
         ListLink(atom,
@@ -30,7 +43,7 @@ def evaluate(atom, *args):
                  ListLink(*args)))
 
 
-def execute(atom, *args):
+def execute(atom, *args, wrap_args=True):
     return ExecutionOutputLink(
         GroundedSchemaNode("py:CogModule.callMethod"),
         ListLink(atom,
@@ -38,13 +51,13 @@ def execute(atom, *args):
                  ListLink(*args)))
 
 
-
 # todo: separate cog module from its usage + static methods should be moved to module (e.g. cog.Execute)
 class CogModule(torch.nn.Module):
     def __init__(self, atom): #todo: atom is optional? if not given, generate by address? string name for concept instead?
         super().__init__()
         self.atom = atom
-        atom.set_value(PredicateNode("cogNet"), PtrValue(self))
+        value = PtrValue(self)
+        atom.set_value(PredicateNode("cogNet"), (value))
 
     @staticmethod
     def callMethod(atom, methodname, args):
@@ -55,8 +68,8 @@ class CogModule(torch.nn.Module):
         obj = valueToPtrValue(value).value()
         return getattr(obj, methodname.name)(args)
 
-    def execute(self, *args):
-        return execute(self.atom, *args)
+    def execute(self, *args, wrap_args=True):
+        return execute(self.atom, *args, wrap_args=wrap_args)
 
     def evaluate(self, *args):
         return evaluate(self.atom, *args)
@@ -65,14 +78,22 @@ class CogModule(torch.nn.Module):
         #print("Args: ", args)
         #todo: check if ListLink
         args = args.out
-        self.cached_result = self.forward(*unpack_args(*args))
+        result = self.forward(*unpack_args(*args))
             #*(cogm.cached_result for cogm in ...)
-        return self.atom
+        if args:
+            atomspace = args[0].atomspace
+        else:
+            atomspace = self.atom.atomspace
+        id = uuid.uuid4()
+        res_atom = atomspace.add_node(types.ConceptNode, 'tmp-result-' + str(id))
+        set_value(res_atom, result)
+        return res_atom
 
     def call_forward_tv(self, args):
-        self.call_forward(args)
+        #print("Args in evaluation link: ", args)
+        res_atom = self.call_forward(args)
         v = torch.mean(self.cached_result)
-        self.atom.truth_value(v, 1.0) #todo???
+        res_atom.truth_value(v, 1.0) #todo???
         return TruthValue(v)
 
     @staticmethod
@@ -85,9 +106,7 @@ class InputModule(CogModule):
     def __init__(self, atom, im):
         super().__init__(atom)
         self.im = im
-    # def set_input(self, im) -- can be a method in cogModule
-    # once called, id of the current input is increase to re-call forward() from execute(),
-    # otherwise cached result can be returned... id trees can be automatically constructed by execute()...
+
     def forward(self):
         return self.im
 
