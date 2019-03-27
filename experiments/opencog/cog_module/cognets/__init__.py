703708880dbd6806d1991d0b70176e0247112748
Anatoly Belikov <awbelikov@gmail.com>
2019-03-27 18:08:18 +0300

    move module .py to cognets

---
+++ experiments/opencog/cog_module/cognets/__init__.py
@@ -0,0 +1,5 @@
+from .module import EVALMODE
+from .module import get_value, set_value, unpack_args
+from .module import evaluate, execute
+from .module import get_value, CogModule, InputModule
+from .module import InheritanceModule, CogModel, TTruthValue
