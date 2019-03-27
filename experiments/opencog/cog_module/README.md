fd2949a1d0abe7a1451602b0425af38abb8cfa38
Anatoly Belikov <awbelikov@gmail.com>
2019-03-27 18:07:40 +0300

    setup.py for cognets

---
+++ experiments/opencog/cog_module/README.md
@@ -0,0 +1,5 @@
+## cognets
+
+**cognets** is pytorch wrapper for opencog
+
+for usage see examples and minst.ipynb
---
+++ experiments/opencog/cog_module/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cognets',
+      version='1.0',
+      description='pytorch wrapper for opencog',
+      author='Anatoly Belikov',
+      author_email='abelikov@singularitynet.io',
+      url='https://github.com/singnet/semantic-vision',
+      packages=['cognets'],
+     )
