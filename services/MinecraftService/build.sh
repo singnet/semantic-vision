e1d254e6ea3408006e36cc911a2c2e60ac5d5bcd
DaddyWesker <avenger15@yandex.ru>
2019-10-07 09:41:07 +0300

    build.sh was updated

--- services/MinecraftService/build.sh
+++ services/MinecraftService/build.sh
@@ -2,3 +2,9 @@ rm *pb2.py
 rm *pb2_grpc.py
 protoc --grpc_out=./ --plugin=protoc-gen-grpc=/usr/local/bin/grpc_python_plugin MinecraftizingService.proto
 protoc --python_out=./ MinecraftizingService.proto
+wget --no-check-certificate https://snet-models.s3.amazonaws.com/semantic-vision/MinecraftService/CycleGan.tar
+tar zxvf CycleGan.tar
+rm CycleGan.tar
+wget --no-check-certificate https://snet-models.s3.amazonaws.com/semantic-vision/MinecraftService/UGATIT.tar
+tar zxvf UGATIT.tar
+rm UGATIT.tar
